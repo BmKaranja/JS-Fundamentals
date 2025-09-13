9420017a06120a3e7673eabc9f1ad1ebbbf0917d
Brian Karanja <bmkaranja001@gmail.com>
2025-09-13 16:56:43 +0300

    Another fix

--- 8-square.js
+++ 8-square.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
 
-const inputs = ["nap", 10, 3];
+const size = parseInt(process.argv[2]);
 
-for (const input of inputs) {
-  const size = parseInt(input);
-  if (isNaN(size)) {
-    console.log("Missing size");
-  } else if (size > 0) {
-    console.log(`\nSize ${size}:`);
-    for (let i = 0; i < size; i++) {
-      console.log("X".repeat(size));
-    }
+if (isNaN(size) || size <= 0) {
+  console.log("Missing size");
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log("X".repeat(size));
   }
 }
